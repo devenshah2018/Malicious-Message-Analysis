613c8d868a8e6d61d56601b6f63c0d533b7318df
devenshah2018 <46404643+devenshah2018@users.noreply.github.com>
2021-05-11 13:31:09 -0700

    Create Sender.java

---
+++ Sender.java
@@ -0,0 +1,28 @@
+public class Sender implements Comparable<Sender>{
+    protected double AccountID;
+    protected double RecipientIPAddress;
+    protected int time; // in minutes
+    
+    public Sender(double AccountID, double RecipientIPAddress, int time){
+        this.AccountID = AccountID;
+        this.RecipientIPAddress = RecipientIPAddress;
+        this.time = time;
+    }
+    
+    public int getTime(){
+        return time;
+    }
+    public double getIP(){
+        return RecipientIPAddress;
+    }
+    
+    @Override
+    public String toString() {
+        return "Sender [Account ID = " + AccountID + "| Recipient ID Address = " + RecipientIPAddress + "| Time = " + time + "]";
+    }
+    
+    @Override
+    public int compareTo(Sender s) {
+        return this.time - s.time;
+    }
+}
